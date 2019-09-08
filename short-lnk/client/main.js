2d9bb07705f20be42fccec73812fd5e09b2c1528
eXCom <rain.tamper@gmail.com>
2019-09-08 14:37:19 +0300

    Lesson 54. Setting Up Routes

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,9 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Login from './../imports/ui/Login';
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={Login}/>
+            <Route path="/signup" component={Signup}/>
+            <Route path="/links" component={Link}/>
+            <Route path="*" component={NotFound}/>
+        </Switch>
+    </BrowserRouter>
+);
 
 Meteor.startup(() => {
-    ReactDOM.render(<Link/>, document.getElementById('app'));
+    ReactDOM.render(routes, document.getElementById('app'));
 });
