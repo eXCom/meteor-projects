f5a1186e6afaaf27612f107560c348dc6716d4b4
eXCom <rain.tamper@gmail.com>
2019-09-08 13:53:11 +0300

    moved to meteor 1.7.0.5

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Signup from '../imports/ui/Signup';
-import Link from '../imports/ui/Link';
+import Signup from './../imports/ui/Signup';
+import Link from './../imports/ui/Link';
 
 Meteor.startup(() => {
     ReactDOM.render(<Link/>, document.getElementById('app'));
