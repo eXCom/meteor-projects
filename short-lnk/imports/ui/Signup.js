0f1289bdb12b853fb547e4efada7551dc67a7c8e
eXCom <rain.tamper@gmail.com>
2019-09-14 23:38:28 +0300

    Lesson 56. Component State and Signup Form

--- short-lnk/imports/ui/Signup.js
+++ short-lnk/imports/ui/Signup.js
@@ -2,11 +2,34 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Signup extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: ''
+        }
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        this.setState({
+            error: 'Something went wrong'
+        });
+    }
+
     render() {
         return (
             <div>
-                <p>Signup component here</p>
-                signup form here
+                <h1>Join short lnk</h1>
+
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" name="email" placeholder="Email" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <button>Create account</button>
+                </form>
 
                 <Link to="/">Login</Link>
             </div>
